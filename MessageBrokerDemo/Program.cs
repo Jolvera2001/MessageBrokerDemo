0e0a7c2340462595f2339916d151f0ea4abd9a47
Jolvera2001 <jolvera6@stedwards.edu>
2024-10-25 20:18:12 -0500

    added receiver project and implemented sender client

--- MessageBrokerDemo/Program.cs
+++ MessageBrokerDemo/Program.cs
@@ -1,3 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using RabbitMQ.Client;
 
-Console.WriteLine("Hello, World!");
+namespace MessageBrokerDemo;
+
+class Program
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost",
+            UserName = "guest",
+            Password = "guest"
+        };
+        
+        // connection and channel
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        
+        // declaring a queue
+        string queueName = "test";
+        channel.QueueDeclare(
+            queue: queueName, 
+            durable: false, 
+            exclusive: false, 
+            autoDelete: false, 
+            arguments: null);
+
+        while (true)
+        {
+            Console.WriteLine($"Enter a Message (or \"exit\" to quit) ");
+            string message = Console.ReadLine();
+
+            if (message.ToLower() == "exit")
+            {
+                break;
+            }
+
+            if (!string.IsNullOrEmpty(message))
+            {
+                var body = Encoding.UTF8.GetBytes(message);
+                
+                channel.BasicPublish(exchange: "",
+                                routingKey: queueName,
+                                basicProperties: null,
+                                body: body);
+                Console.WriteLine("Sent message: ", message);
+            }
+        }
+    }
+}
